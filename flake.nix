0fb4ae30e18757bad36f45d61b6e6fa02df57da5
Ian-Woo Kim <ianwookim@gmail.com>
2024-07-30 09:12:04 -0700

    flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "ghc-worker";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      hpkgs = pkgs.haskellPackages;
+      hsenv = hpkgs.ghcWithPackages (p: []);
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "ghc-worker-test-shell";
+        buildInputs = [
+          hsenv
+        ];
+        shellHook = ''
+          export PS1="\n[ghc-worker-test-shell:\w]$ \0"
+        '';
+      };
+    });
+}
