6858fe6b97481d4cb5afb5095086e90f4656bcb7
Ian-Woo Kim <ianwookim@gmail.com>
2024-08-07 07:30:37 -0700

    Chunked message transfer!

--- myplugin/src/Message.hs
+++ myplugin/src/Message.hs
@@ -10,10 +10,12 @@ module Message
     ConsoleOutput (..),
   ) where
 
+import Control.Monad (replicateM)
 import Data.Binary (Binary (..), encode, decode)
 import Data.ByteString (ByteString (..))
 import qualified Data.ByteString.Char8 as C
 import qualified Data.ByteString.Lazy as L
+import Data.Foldable (traverse_)
 import Data.Int (Int32 (..))
 import Network.Socket (Socket (..))
 import Network.Socket.ByteString (recv, sendAll)
@@ -23,16 +25,37 @@ data Msg = Msg
     msgPayload :: ByteString
   }
 
+chunkSize :: Int
+chunkSize = 4096
+
+toChunks :: Int -> ByteString -> [ByteString]
+toChunks bytes bs0 = go id bs0 []
+  where
+    go !acc !bs =
+      let (chunk, bs') = C.splitAt bytes bs
+       in if C.length bs' < bytes
+          then (acc . (chunk:) . (bs':))
+          else go (acc . (chunk:)) bs'
+
 sendMsg :: Socket -> Msg -> IO ()
 sendMsg s (Msg n payload) = do
   sendAll s (L.toStrict (encode n))
-  sendAll s payload
+  let chunks = toChunks chunkSize payload
+  -- putStrLn $ "sendMsg: " ++ show n ++ " bytes " ++ show (length chunks)
+  traverse_ (sendAll s) chunks
 
 recvMsg :: Socket -> IO Msg
 recvMsg s = do
   msg_n <- recv s 4
-  let n = decode (L.fromStrict msg_n)
-  payload <- recv s (fromIntegral n)
+  let n :: Int32
+      n = decode (L.fromStrict msg_n)
+      n' :: Int
+      n' = fromIntegral n
+      (q, r) = n' `divMod` chunkSize
+  -- putStrLn $ "recvMsg: " ++ show n' ++ " bytes " ++ show (q, r)
+  ps <- replicateM q (recv s chunkSize)
+  p <- recv s r
+  let payload = mconcat (ps ++ [p])
   pure (Msg n payload)
 
 wrapMsg :: (Binary a) => a -> Msg
