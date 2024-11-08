8380787e08fb34261e488e8f665e71bd947c1c62
Torsten Schmits <git@tryp.io>
2024-11-08 16:04:49 +0100

    Exfiltrate compilation artifacts
    * Move pipeline creation into local code
    * Return ModIface and the bytecode Linkable instead of discarding the entire HomeModInfo
    * Extract ABI hash from that ModIface
    * Add an override for FinderCache that records some statistics

--- plugin/src/Internal/AbiHash.hs
+++ plugin/src/Internal/AbiHash.hs
@@ -5,7 +5,7 @@ import GHC.Driver.Config.Logger (initLogFlags)
 import GHC.Driver.Env (HscEnv (..))
 import GHC.Driver.Session (targetProfile)
 import GHC.Iface.Binary (CheckHiWay (IgnoreHiWay), TraceBinIFace (QuietBinIFace), readBinIface)
-import GHC.Unit.Module.ModIface (mi_final_exts, mi_mod_hash)
+import GHC.Unit.Module.ModIface (mi_final_exts, mi_mod_hash, ModIface)
 import GHC.Utils.Logger (LogFlags (..), log_default_dump_context)
 import GHC.Utils.Outputable (ppr, renderWithContext)
 import System.FilePath (dropExtension)
@@ -17,16 +17,20 @@ data AbiHash =
   }
   deriving stock (Eq, Show)
 
+showAbiHash :: HscEnv -> ModIface -> String
+showAbiHash HscEnv {hsc_dflags} iface =
+  dump hsc_dflags (mi_mod_hash (mi_final_exts iface))
+  where
+    dump dflags = renderWithContext (log_default_dump_context (initLogFlags dflags)) . ppr
+
 readAbiHash ::
   MonadIO m =>
   HscEnv ->
   Maybe String ->
-  m (Maybe AbiHash)
-readAbiHash HscEnv {hsc_dflags, hsc_NC} (Just path) = do
+  m (Maybe (ModIface, AbiHash))
+readAbiHash hsc_env@HscEnv {hsc_dflags, hsc_NC} (Just path) = do
   let hi_file = dropExtension path
   iface <- liftIO $ readBinIface (targetProfile hsc_dflags) hsc_NC IgnoreHiWay QuietBinIFace hi_file
-  pure (Just (AbiHash {path, hash = dump hsc_dflags (mi_mod_hash (mi_final_exts iface))}))
-  where
-    dump dflags = renderWithContext (log_default_dump_context (initLogFlags dflags)) . ppr
+  pure (Just (iface, AbiHash {path, hash = showAbiHash hsc_env iface}))
 
 readAbiHash _ _ = pure Nothing
