3bba43de72abab9d45e10c4d7087be0f9158b9ec
Torsten Schmits <git@tryp.io>
2025-04-03 15:42:54 +0200

    add nix shell

---
+++ shell.nix
@@ -0,0 +1,8 @@
+let
+  pkgs = import (fetchTarball {
+    url = "https://github.com/nixos/nixpkgs/archive/e913ae340076bbb73d9f4d3d065c2bca7caafb16.tar.gz";
+    sha256 = "07qjibn85hc6p8c7lwg00fwpyqjlmaxncz9wa2l6qpy1hsk51k8f";
+  }) {};
+in pkgs.mkShell {
+  packages = [pkgs.haskell.compiler.ghc910 pkgs.zlib pkgs.zlib.dev pkgs.snappy pkgs.protobuf];
+}
