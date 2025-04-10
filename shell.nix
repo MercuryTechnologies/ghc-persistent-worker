2472e359dba91230381d8162b724369c1e5d281f
Torsten Schmits <git@tryp.io>
2025-04-10 18:31:43 +0200

    add more shell packages
    Change-Id: I55dbc8ccf042b9645f055759bb6404fb8c72572b

--- shell.nix
+++ shell.nix
@@ -3,16 +3,29 @@ let
     url = "https://github.com/nixos/nixpkgs/archive/ba487dbc9d04e0634c64e3b1f0d25839a0a68246.tar.gz";
     sha256 = "0wr8pnx2bkr88vxv3aqa9y9vrcixicm2vahws7i2kvcpy8mnb4sr";
   }) {};
-in pkgs.mkShell {
-  ghc_dir = "${pkgs.haskell.compiler.ghc910}";
-  packages = [
-    pkgs.haskell.compiler.ghc910
-    pkgs.haskell.packages.ghc910.cabal-install
-    pkgs.zlib
-    pkgs.zlib.dev
+
+  libs = [
     pkgs.zstd
+    pkgs.xz
+    pkgs.bzip2
+    pkgs.zlib
+  ];
+
+
+in pkgs.mkShell {
+
+  ghc_dir = "${pkgs.haskell.compiler.ghc910}";
+
+  packages = [
+    (pkgs.haskell.packages.ghc910.ghcWithPackages (g: [g.cabal-install]))
+    pkgs.zlib.dev
     pkgs.snappy
     pkgs.protobuf
     pkgs.git
-  ];
+  ] ++ libs;
+
+  shellHook = ''
+  export LD_LIBRARY_PATH="${pkgs.lib.makeSearchPathOutput "out" "lib" libs}:$LD_LIBRARY_PATH"
+  '';
+
 }
