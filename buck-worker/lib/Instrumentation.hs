2c0db6afe08ec358719fd3ebe502de213ca2ca90
Torsten Schmits <git@tryp.io>
2025-04-14 19:20:16 +0200

    document `Instrumentation`
    Change-Id: I22c516d7e3016178a0e7669a5561efecb9ff662c

--- buck-worker/lib/Instrumentation.hs
+++ buck-worker/lib/Instrumentation.hs
@@ -14,17 +14,26 @@ import Prelude hiding (log)
 import qualified Proto.Instrument as Instr
 import Proto.Instrument_Fields qualified as Instr
 
+-- | Rudimentary dummy state for instrumentation, counting concurrently compiling sessions.
 data WorkerStatus =
   WorkerStatus {
     active :: Int
   }
 
+-- | Callbacks passed to GHC request handlers that trigger instrumentation events.
 data Hooks =
   Hooks {
+    -- | A module compilation is started.
+    -- If it can be determined at this point, the argument contains the file name.
+    -- This is not available in multiplexer mode.
     compileStart :: Maybe Target -> IO (),
+
+    -- | A module compilation has finished.
+    -- If the job was successful, the argument contains 'Just' the stderr lines and the exit code, otherwise 'Nothing'.
     compileFinish :: Maybe ([String], Int32) -> IO ()
   }
 
+-- | Dummy implementation of 'Hooks'.
 hooksNoop :: Hooks
 hooksNoop =
   Hooks {
@@ -32,9 +41,11 @@ hooksNoop =
     compileFinish = const (pure ())
   }
 
+-- | A request handler that is aware of instrumentation.
 newtype InstrumentedHandler =
   InstrumentedHandler { create :: Hooks -> GrpcHandler }
 
+-- | Register a newly started job by incrementing the active job count.
 startJob ::
   MVar WorkerStatus ->
   IO ()
@@ -44,6 +55,7 @@ startJob var =
     dbg ("Starting job, now " ++ show new ++ " active")
     pure ws {active = new}
 
+-- | Decrement the active job count.
 finishJob ::
   MVar WorkerStatus ->
   IO ()
@@ -53,11 +65,13 @@ finishJob var = do
     dbg ("Finishing job, now " ++ show new ++ " active")
     pure ws {active = new}
 
+-- | Construct a grapesy message for a "compilation started" event.
 messageCompileStart :: String -> Proto Instr.CompileStart
 messageCompileStart target =
   defMessage
     & Instr.target .~ Text.pack target
 
+-- | Construct a grapesy message for a "compilation finished" event.
 messageCompileEnd :: String -> Int -> String -> Proto Instr.CompileEnd
 messageCompileEnd target exitCode err =
   defMessage
@@ -65,6 +79,12 @@ messageCompileEnd target exitCode err =
     & Instr.exitCode .~ fromIntegral exitCode
     & Instr.stderr .~ Text.pack err
 
+-- | Run a 'GrpcHandler' with instrumentation enabled.
+--
+-- This consists of adapting the active job count and sending messages to the gRPC client running the instrumentation
+-- app.
+-- The handler is initialized by passing 'Hooks' to its constructor function, which contains callbacks for sending
+-- additional messages.
 withInstrumentation ::
   Chan (Proto Instr.Event) ->
   MVar WorkerStatus ->
@@ -90,6 +110,7 @@ withInstrumentation instrChan status handler =
             Instr.compileStart .~
               messageCompileStart target.get
 
+    -- Note: This is WIP.
     compileFinish =
       traverse_ \ (output, exitCode) ->
         writeChan instrChan $
@@ -97,6 +118,8 @@ withInstrumentation instrChan status handler =
             Instr.compileEnd .~
               messageCompileEnd "" (fromIntegral exitCode) (unlines output)
 
+-- | Construct a 'GrpcHandler' by passing functioning 'Hooks' to an 'InstrumentedHandler' if the third argument contains
+-- 'Just' a message channel, or passing no-op 'Hooks' otherwise.
 toGrpcHandler ::
   InstrumentedHandler ->
   MVar WorkerStatus ->
