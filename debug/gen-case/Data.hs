eda784ae407d0797c042d0aab21a785b0152a288
Torsten Schmits <git@tryp.io>
2025-05-02 15:34:00 +0200

    experiment with ghc-debug
    Change-Id: I6b7549cbc36baf53f1287092dbef99e5aa90d98b

---
+++ debug/gen-case/Data.hs
@@ -0,0 +1,29 @@
+module Data where
+
+import Control.Concurrent (MVar, newMVar)
+
+data TestB =
+  TestB (MVar Int) (Maybe String)
+
+instance Show TestB where
+  show (TestB _ s) = show s
+
+data TestA =
+  TestA (Maybe TestB) TestB
+  deriving stock (Show)
+
+mkB1 :: IO TestB
+mkB1 = do
+  mv <- newMVar 1
+  pure (TestB mv (Just "one"))
+
+mkB2 :: IO TestB
+mkB2 = do
+  mv <- newMVar 2
+  pure (TestB mv (Just "two"))
+
+mkA :: IO TestA
+mkA = do
+  b1 <- mkB1
+  b2 <- mkB2
+  pure (TestA (Just b1) b2)
---
+++ debug/gen-case/Main.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import Control.Concurrent (MVar, modifyMVar_, newMVar, readMVar, threadDelay)
+import Data (TestA (TestA), mkA, mkB1, mkB2)
+import Data.Foldable (traverse_)
+import GHC.Debug.Stub (withGhcDebug)
+
+setup :: IO (MVar [TestA])
+setup = do
+  b1 <- mkB1
+  b2 <- mkB2
+  a1 <- mkA
+  newMVar [a1, TestA (Just b1) b2]
+{-# noinline setup #-}
+
+main :: IO ()
+main = do
+  withGhcDebug do
+    mv <- setup
+    b2 <- mkB2
+    modifyMVar_ mv \ as -> pure (TestA Nothing b2 : as)
+    threadDelay 10_000_000_000
+    readMVar mv >>= traverse_ \ a -> print a
