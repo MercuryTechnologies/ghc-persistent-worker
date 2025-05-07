7b246149b013666fc0fe48e29ac56db762958148
Sjoerd Visscher <sjoerd@w3future.com>
2025-05-07 14:47:52 +0200

    Make genproto.sh work in nix
    Change-Id: I23fbcdd7c2257bb62f3e9e46f7da77904a3786b0

--- buck-worker/genproto.sh
+++ buck-worker/genproto.sh
@@ -9,4 +9,8 @@ protoc --plugin=protoc-gen-haskell=$(which proto-lens-protoc) \
 
 # Imports are generated with re-exported modules, which we currently don't support in the buck2 build.
 # Remove the re-exported module prefixes so the original modules are imported.
-find "$SCRIPT_DIR/lib/Proto" -type f -exec sed -i '' -E 's/Data\.ProtoLens\.Runtime\.[^ ]* as //g' {} +
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  find "$SCRIPT_DIR/lib/Proto" -type f -exec sed -i '' -E 's/Data\.ProtoLens\.Runtime\.[^ ]* as //g' {} +
+else
+  find "$SCRIPT_DIR/lib/Proto" -type f -exec sed -i -E 's/Data\.ProtoLens\.Runtime\.[^ ]* as //g' {} +
+fi
