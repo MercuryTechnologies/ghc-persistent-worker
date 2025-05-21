d07c170d99f61306b8a67a6c9ef4547c5483f7bd
Sjoerd Visscher <sjoerd@w3future.com>
2025-05-21 14:10:12 +0200

    Fix genproto.sh
    Change-Id: I20c4cc4d53cec0a464576f0d1d384b3a4ccc91c5

--- proto/genproto.sh
+++ proto/genproto.sh
@@ -4,13 +4,9 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 protoc --plugin=protoc-gen-haskell=$(which proto-lens-protoc) \
-  -I="$SCRIPT_DIR" --haskell_out="$SCRIPT_DIR/lib" \
+  -I="$SCRIPT_DIR" --haskell_out="$SCRIPT_DIR/src" \
   "$SCRIPT_DIR/worker.proto" "$SCRIPT_DIR/instrument.proto"
 
 # Imports are generated with re-exported modules, which we currently don't support in the buck2 build.
 # Remove the re-exported module prefixes so the original modules are imported.
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  find "$SCRIPT_DIR/lib/Proto" -type f -exec sed -i '' -E 's/Data\.ProtoLens\.Runtime\.[^ ]* as //g' {} +
-else
-  find "$SCRIPT_DIR/lib/Proto" -type f -exec sed -i -E 's/Data\.ProtoLens\.Runtime\.[^ ]* as //g' {} +
-fi
+find "$SCRIPT_DIR/src/Proto" -type f -exec perl -pi -E 's/Data\.ProtoLens\.Runtime\.[^ ]* as //g' {} +
