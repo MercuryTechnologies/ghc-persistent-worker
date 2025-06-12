a5aefb46eaa5f5118b86a096e5fea723c98c32b0
Sjoerd Visscher <sjoerd@w3future.com>
2025-06-12 17:10:58 +0200

    Grpc: We're not going to use grpc metadata, so declare NoMetadata for all methods in one go.
    Change-Id: I23a413748c53830cf00f7d24ef7d15a21bf156aa

--- proto/src/BuckWorker.hs
+++ proto/src/BuckWorker.hs
@@ -12,18 +12,10 @@ import Network.GRPC.Common.Protobuf (Protobuf)
 import Proto.Worker
 import Proto.Instrument
 
-type instance RequestMetadata (Protobuf Worker "exec") = NoMetadata
-type instance ResponseInitialMetadata (Protobuf Worker "exec") = NoMetadata
-type instance ResponseTrailingMetadata (Protobuf Worker "exec") = NoMetadata
+type instance RequestMetadata (Protobuf Worker _) = NoMetadata
+type instance ResponseInitialMetadata (Protobuf Worker _) = NoMetadata
+type instance ResponseTrailingMetadata (Protobuf Worker _) = NoMetadata
 
-type instance RequestMetadata (Protobuf Worker "execute") = NoMetadata
-type instance ResponseInitialMetadata (Protobuf Worker "execute") = NoMetadata
-type instance ResponseTrailingMetadata (Protobuf Worker "execute") = NoMetadata
-
-type instance RequestMetadata (Protobuf Instrument "notifyMe") = NoMetadata
-type instance ResponseInitialMetadata (Protobuf Instrument "notifyMe") = NoMetadata
-type instance ResponseTrailingMetadata (Protobuf Instrument "notifyMe") = NoMetadata
-
-type instance RequestMetadata (Protobuf Instrument "setOptions") = NoMetadata
-type instance ResponseInitialMetadata (Protobuf Instrument "setOptions") = NoMetadata
-type instance ResponseTrailingMetadata (Protobuf Instrument "setOptions") = NoMetadata
+type instance RequestMetadata (Protobuf Instrument _) = NoMetadata
+type instance ResponseInitialMetadata (Protobuf Instrument _) = NoMetadata
+type instance ResponseTrailingMetadata (Protobuf Instrument _) = NoMetadata
