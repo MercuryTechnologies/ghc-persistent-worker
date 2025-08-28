182b7c4cc55de768a1cb92258c089dd6f83506c4
Torsten Schmits <git@tryp.io>
2025-08-28 19:35:40 +0200

    Fix bytecode laziness when restoring HPTs from cache
    Change-Id: I3e14a6d89502e3ee7ee38b75ca5363f8a14a8767

--- internal/src/Internal/Cache/Hpt.hs
+++ internal/src/Internal/Cache/Hpt.hs
@@ -7,26 +7,30 @@ import Control.Monad (foldM)
 import Control.Monad.IO.Class (liftIO)
 import Data.Foldable (toList)
 import Data.Function (on)
+import Data.Functor ((<&>))
 import Data.List.NonEmpty (NonEmpty ((:|)), groupBy)
-import GHC (DynFlags (..), Ghc, GhcException (..), ModIface, ModIface_ (..), ModuleName, mkModule, moduleName)
+import Data.Time (getCurrentTime)
+import Data.Traversable (for)
+import GHC (DynFlags (..), Ghc, GhcException (..), ModIface, ModIface_ (..), ModLocation (..), ModuleName, mkModule)
 import GHC.Data.Maybe (MaybeErr (..))
 import GHC.Driver.DynFlags (GhcMode (..))
 import GHC.Driver.Env (HscEnv (..), hscActiveUnitId, hscSetActiveUnitId, hscUpdateFlags, hscUpdateHPT, hsc_HPT)
-import GHC.Driver.Main (initModDetails)
+import GHC.Driver.Main (initModDetails, initWholeCoreBindings)
 import GHC.Iface.Errors.Ppr (readInterfaceErrorDiagnostic)
 import GHC.Iface.Load (readIface)
-import GHC.Linker.Types (Linkable)
+import GHC.Linker.Types (Linkable (..))
 import GHC.Platform.Ways (Way (..), addWay)
 import GHC.Types.Unique.DFM (addToUDFM, elemUDFM)
 import GHC.Unit (Definite (..), GenUnit (..))
 import GHC.Unit.Env (UnitEnv (..), unitEnv_member)
-import GHC.Unit.Home.ModInfo (HomeModInfo (..), HomeModLinkable (..), addToHpt, emptyHomeModInfoLinkable)
+import GHC.Unit.Home.ModInfo (HomeModInfo (..), HomeModLinkable (..))
 import GHC.Unit.Module.ModDetails (ModDetails (..))
 import GHC.Utils.Outputable (ppr, ($+$))
+import GHC.Unit.Module.WholeCoreBindings (WholeCoreBindings (..))
+import GHC.Utils.Misc (modificationTimeIfExists)
 import GHC.Utils.Panic (throwGhcExceptionIO)
 import Internal.Log (Log (..), logDebug)
 import Prelude hiding (log)
-import System.IO.Unsafe (unsafeInterleaveIO)
 import Types.CachedDeps (
   CachedDeps (..),
   CachedInterface (..),
@@ -38,17 +42,10 @@ import Types.CachedDeps (
 -- This preprocessor variable indicates that we're building with a GHC that has the final version of the oneshot
 -- bytecode patch.
 #if defined(MWB_2025_07) || MIN_VERSION_GLASGOW_HASKELL(9,12,0,0)
-import GHC (ModLocation (..))
-import GHC.Driver.Main (loadIfaceByteCode)
+import GHC.Linker.Types (LinkablePart (..))
+#else
+import GHC.Linker.Types (Unlinked (..))
 #endif
-
-add_iface_to_hpt :: ModIface -> ModDetails -> HscEnv -> HscEnv
-add_iface_to_hpt iface details =
-  hscUpdateHPT $ \ hpt ->
-    addToHpt hpt (moduleName (mi_module iface'))
-    (HomeModInfo iface' details emptyHomeModInfoLinkable)
-  where
-    iface' = iface { mi_extra_decls = Nothing }
 
 -- | Load bytecode from an interface.
 -- Used only for modules missing from the current target's HPT when restoring the Buck cache after restarting a build.
@@ -57,11 +54,10 @@ add_iface_to_hpt iface details =
 -- eventually.
 -- For example, the source file is used to add debug info and find foreign export stubs.
 loadCachedByteCode :: HscEnv -> FilePath -> ModIface -> ModDetails -> IO (Maybe Linkable)
--- The function 'loadIfaceByteCode' is only available in the final version of the oneshot-bytecode patch, which isn't
--- part of the MWB branch of GHC.
-#if defined(MWB_2025_07) || MIN_VERSION_GLASGOW_HASKELL(9,12,0,0)
 loadCachedByteCode hsc_env ifaceFile iface details =
-   sequence (loadIfaceByteCode hsc_env' iface location (md_types details))
+  for core_bindings \ wcb -> do
+    linkable <- bcoLinkable [CoreBindings wcb]
+    initWholeCoreBindings hsc_env iface details linkable
    where
     location =
       ModLocation {
@@ -73,10 +69,23 @@ loadCachedByteCode hsc_env ifaceFile iface details =
         ml_hie_file = error "loadCachedByteCode"
       }
 
-    hsc_env' = add_iface_to_hpt iface details hsc_env
+    core_bindings =
+      mi_extra_decls <&> \ wcb_bindings ->
+#if defined(MWB_2025_07) || MIN_VERSION_GLASGOW_HASKELL(9,12,0,0)
+        WholeCoreBindings {wcb_mod_location = location, wcb_foreign = mi_foreign, ..}
 #else
-loadCachedByteCode _ _ _ _ =
-  pure Nothing
+        WholeCoreBindings {wcb_mod_location = location, ..}
+#endif
+      where
+        ModIface {mi_module = wcb_module, ..} = iface
+
+    bcoLinkable parts = do
+      if_time <- modificationTimeIfExists (ml_hi_file location)
+      time <- maybe getCurrentTime pure if_time
+#if defined(MWB_2025_07) || MIN_VERSION_GLASGOW_HASKELL(9,12,0,0)
+      return $! Linkable time (mi_module iface) parts
+#else
+      return $! LM time (mi_module iface) parts
 #endif
 
 -- | If the given module name is missing from the HPT, load the given interface from disk and store it in the module's
@@ -101,7 +110,7 @@ loadCachedDep log name hsc_env ifaceFile =
       logDebug log ("Loading HPT module from cache: " ++ ifaceFile)
       hm_iface <- loadIface
       hm_details <- initModDetails hsc_env hm_iface
-      homeMod_bytecode <- unsafeInterleaveIO $ loadCachedByteCode hsc_env ifaceFile hm_iface hm_details
+      homeMod_bytecode <- loadCachedByteCode hsc_env ifaceFile hm_iface hm_details
       let new = addToUDFM hpt name HomeModInfo {
         hm_iface,
         hm_linkable = HomeModLinkable {homeMod_object = Nothing, homeMod_bytecode},
@@ -145,7 +154,7 @@ loadCachedDeps log CachedDeps {home_unit, project} hsc_env0 = do
   where
     setDyn = hscUpdateFlags \ d -> d {ghcMode = CompManager, targetWays_ = addWay WayDyn (targetWays_ d)}
 
-    -- If the unit isn't part of the module graph, it wasn't built by a worker, since it would have been loaded in the
+    -- If the unit isn't present in the unit env, it wasn't built by a worker, since it would have been loaded in the
     -- metadata restoration step.
     loadDepUnit hsc_env mods@(CachedProjectDep {package = JsonFs uid} :| _) =
       if unitEnv_member uid hsc_env.hsc_unit_env.ue_home_unit_graph
