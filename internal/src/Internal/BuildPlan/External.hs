6c05380ad84191e5cb1cc9355eedbf339ab84d34
Ian-Woo Kim <ianwookim@gmail.com>
2026-05-13 01:09:43 +0000

    lookupImport produces Left error_message on failure in finding import module
    Change-Id: I1c7abd4dae2e836b20708b840a42eb40dbdf8cac

--- internal/src/Internal/BuildPlan/External.hs
+++ internal/src/Internal/BuildPlan/External.hs
@@ -9,12 +9,20 @@ import qualified Data.Set as Set
 import Data.Set (Set)
 import Data.Traversable (mapAccumM)
 import GHC (GenLocated (..), Located, PkgQual)
+import GHC.Data.Maybe (rightToMaybe)
 import GHC.Driver.Env (HscEnv (..))
+import GHC.Driver.Errors.Types (DriverMessage (..))
+import GHC.Iface.Errors (cannotFindModule)
+import GHC.Iface.Errors.Types (IfaceMessage (Can'tFindInterface), InterfaceLookingFor (LookingForModule))
+import GHC.Types.Error (Messages, singleMessage)
+import GHC.Types.SrcLoc (SrcSpan (..))
 import GHC.Types.Unique.Map (UniqMap, lookupUniqMap)
 import GHC.Unit (Definite (..), GenModule (..), GenUnit (..))
 import GHC.Unit.Finder (FindResult (..), findImportedModule)
 import GHC.Unit.Module (ModuleName (..), UnitId (..), unitIdString)
 import GHC.Unit.Module.ModSummary (ModSummary (..))
+import GHC.Utils.Error (mkPlainErrorMsgEnvelope)
+import Language.Haskell.Syntax.ImpExp (IsBootInterface (NotBoot))
 import Types.BuildPlan (BuildPlanEnv (..), ModuleKey (..), PackageKey (..), summaryModuleKey)
 
 -- | A lookup table for external modules shared across units.
@@ -31,22 +39,29 @@ lookupImport ::
   Set UnitId ->
   PkgQual ->
   ModuleName ->
-  IO (Maybe (Maybe (UnitId, ModuleName)))
-lookupImport hsc_env homeUnitIds qual name =
+  SrcSpan ->
+  IO (Either (Messages DriverMessage) (Maybe (UnitId, ModuleName)))
+lookupImport hsc_env homeUnitIds qual name loc =
   findImportedModule hsc_env name qual <&> \case
     Found _ Module {moduleName, moduleUnit = RealUnit (Definite unit)}
       | Set.member unit homeUnitIds
-      -> Just Nothing
+      -> Right Nothing
       | otherwise
-      -> Just (Just (unit, moduleName))
-    _ -> Nothing
+      -> Right (Just (unit, moduleName))
+    fail_detail ->
+      Left $
+        singleMessage $
+          mkPlainErrorMsgEnvelope loc $
+            DriverInterfaceError $
+              Can'tFindInterface (cannotFindModule hsc_env name fail_detail) $
+                LookingForModule name NotBoot -- boot module is not supported here yet. TODO: support boot
 
 moduleImport ::
   BuildPlanEnv ->
   ExternalCache ->
   (PkgQual, Located ModuleName) ->
   IO (ExternalCache, Maybe (UnitId, ModuleName))
-moduleImport BuildPlanEnv {hsc_env, homeUnitIds} cache (qual, L _ name) =
+moduleImport BuildPlanEnv {hsc_env, homeUnitIds} cache (qual, L loc name) =
   maybe cacheMiss cacheHit (cache !? (qual, name))
   where
     -- If the import is cached, use it for the deps directly.
@@ -56,7 +71,7 @@ moduleImport BuildPlanEnv {hsc_env, homeUnitIds} cache (qual, L _ name) =
     -- If it can't be found, return 'Nothing'.
     -- We don't want to validate imports, since that already happened during downsweep.
     -- If the imported module is part of the home unit, store and return 'Nothing' to avoid further lookups.
-    cacheMiss = fmap join . traverse found <$> lookupImport hsc_env homeUnitIds qual name
+    cacheMiss = fmap (join . rightToMaybe) . traverse found <$> lookupImport hsc_env homeUnitIds qual name loc
 
     found result = (Map.insert (qual, name) result cache, result)
 
