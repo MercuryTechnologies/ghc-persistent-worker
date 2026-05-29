41c8301dda0de6a0397034a751932bc3c4c14b9e
Ian-Woo Kim <ianwookim@gmail.com>
2026-05-29 01:03:37 +0000

    Build plan import failure property test.
    With missing module imports, the buildPlanForTargets function
    should report errors, which have the same number of occurrences
    as the number of the missing imports.
    
    Change-Id: I9e1ac8b4569007de2a28b130b9c0d07370ebe824

---
+++ ghc-worker/test/BuildPlanTest/Test2.hs
@@ -0,0 +1,79 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+
+--
+-- Try to make build plan file for an example with missing deps.
+-- The test should show all the errors.
+-- Check make mode and one-shot mode.
+--
+module BuildPlanTest.Test2 where
+
+import Control.Monad.Catch (MonadCatch (catch))
+import Control.Monad.IO.Class (liftIO)
+import Data.ByteString.Char8 (ByteString, pack)
+import Data.Foldable (toList)
+import Data.List.NonEmpty (NonEmpty)
+import qualified Data.Set as Set
+import Data.Set (Set)
+import GHC (DynFlags (..), Ghc, GhcMode (..), Target (..))
+import GHC.Driver.Env (hscUpdateFlags)
+import GHC.Driver.Monad (modifySession)
+import GHC.Types.Error (Messages (..))
+import GHC.Types.SourceError (SourceError (..))
+import GHC.Unit (UnitId, stringToUnitId)
+import Hedgehog (forAll, property, withTests, (===))
+import qualified Hedgehog.Gen as Gen (int)
+import Hedgehog.Range (linear)
+import Internal.BuildPlan (buildPlanForTargets)
+import Prelude hiding (log)
+import System.FilePath ((</>))
+import Test.PackageDb (ModuleSpec (..), UnitSpec (..), moduleSpec)
+import Test.Run (transientSession, withTemp)
+import Test.Target (fileUnitTargets, ghcOptions)
+import Test.Tasty (TestTree)
+import Test.Tasty.Hedgehog (testProperty)
+import Types.Args (BuildPlanField (..), buildPlanAll)
+import Types.BuildPlan (BuildPlan (..))
+
+unit1 :: UnitId
+unit1 = stringToUnitId "unit1"
+
+unit1Modules :: Int -> NonEmpty ModuleSpec
+unit1Modules numMissingImports =
+  let missingImports :: [ByteString]
+      missingImports =  map (\n -> "import NonExistent" <> pack (show n)) [1..numMissingImports]
+      srcTxt =
+        ["module U1M1 where"] ++
+        missingImports ++
+        [
+          "u1m1 :: Int",
+          "u1m1 = 5"
+        ]
+   in [moduleSpec "U1M1" srcTxt]
+
+unit1Spec :: Int -> UnitSpec
+unit1Spec numMissingImports =
+  UnitSpec {name = "unit1", deps = [], modules = unit1Modules numMissingImports}
+
+fields :: Set BuildPlanField
+fields = Set.fromList (toList buildPlanAll)
+
+runBuildPlan :: NonEmpty Target -> Ghc BuildPlan
+runBuildPlan targets = do
+  modifySession (hscUpdateFlags \ d -> d {ghcMode = MkDepend})
+  buildPlanForTargets fields (toList targets)
+
+-- | Run build plan JSON generation and should get errors from non-existent module imports
+--   The number of errors should be matched with the number of errorneous imports.
+test_buildPlan :: TestTree
+test_buildPlan =
+  withTemp "build-plan-make" \ tmpResource -> do
+    testProperty "build plan import errors" $ withTests 10 $ property do
+      numMissingImports <- forAll $ Gen.int (linear 1 10)
+      tmp <- liftIO tmpResource
+      targets <- liftIO $ fileUnitTargets (tmp </> "src") (unit1Spec numMissingImports)
+      numErrs <- transientSession (ghcOptions unit1 []) do
+        catch (runBuildPlan targets >> pure 0) \(SourceError msg) -> do
+          let errs = toList (getMessages msg)
+          pure (length errs)
+      numErrs === numMissingImports
