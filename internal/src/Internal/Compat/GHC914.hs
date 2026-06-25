860317bc365663f5f96f67cfe2480900afc4d0b8
Facundo Domínguez <facundo.dominguez@tweag.io>
2026-06-25 21:03:57 +0000

    Update the module graph with per-module flags
    The ModuleGraph is updated with a traversal that only modifies the
    ModSummaries that have perModuleFlags.
    
    This traversal is implemented in different ways for GHC 9.14 and older,
    as the interface of the ModuleGraph is fluctuating.
    
    Change-Id: Icfff9556ffca9d9211d4791ea9a81da06d857580

--- internal/src/Internal/Compat/GHC914.hs
+++ internal/src/Internal/Compat/GHC914.hs
@@ -2,16 +2,17 @@
 
 module Internal.Compat.GHC914 where
 
-import GHC (Located, ModIface, ModuleGraph, ModuleName, PkgQual)
+import GHC (Located, ModIface, ModSummary, ModuleGraph, ModuleName, PkgQual)
 import GHC.Driver.Env (HscEnv (..))
 import GHC.Iface.Syntax (IfaceBindingX, IfaceMaybeRhs, IfaceTopBndrInfo)
-import GHC.Unit.Module.Graph (NodeKey)
+import GHC.Unit.Module.Graph (ModuleGraphNode(..), NodeKey)
 
 #if MIN_VERSION_GLASGOW_HASKELL(9,14,0,0)
 
 import Data.Functor ((<&>))
 import GHC.Unit.Env (UnitEnv (..))
-import GHC.Unit.Module.Graph (ModuleNodeEdge, edgeTargetKey, mkNormalEdge)
+import GHC.Unit.Module.Graph
+    (ModuleNodeEdge, ModuleNodeInfo(..), edgeTargetKey, mkNormalEdge, mgMapM)
 import GHC.Unit.Module.ModIface (IfaceSimplifiedCore (..), set_mi_simplified_core)
 import GHC.Unit.Module.WholeCoreBindings (emptyIfaceForeign)
 
@@ -35,7 +36,22 @@ setExtraDecls new =
   set_mi_simplified_core $ new <&> \ mi_sc_extra_decls ->
     IfaceSimplifiedCore {mi_sc_foreign = emptyIfaceForeign, ..}
 
+-- | Like 'mapMG', but monadic.
+mapMGM :: (ModSummary -> IO ModSummary) -> ModuleGraph -> IO ModuleGraph
+mapMGM f = mgMapM $ \mni -> case mni of
+  ModuleNodeCompile ms -> ModuleNodeCompile <$> f ms
+  _ -> pure mni
+
 #else
+
+import Control.Monad (forM)
+import GHC.Unit.Module.Graph (ModuleGraph(..))
+
+#if defined(FIXED_NODES)
+
+import GHC.Unit.Module.Graph (ModuleNodeInfo(..))
+
+#endif
 
 #if defined(MWB)
 
@@ -67,5 +83,32 @@ edgeTarget = id
 
 textualImports :: (PkgQual, Located ModuleName) -> (PkgQual, Located ModuleName)
 textualImports = id
+
+#if defined(FIXED_NODES)
+
+mgMapM :: (ModuleNodeInfo -> IO ModuleNodeInfo) -> ModuleGraph -> IO ModuleGraph
+mgMapM f mg = do
+  mgns <- forM (mg_mss mg) $ \mgn -> case mgn of
+      ModuleNode deps mni  -> ModuleNode deps <$> f mni
+      _ -> pure mgn
+  pure mg { mg_mss = mgns }
+
+-- | Like 'mapMG', but monadic.
+mapMGM :: (ModSummary -> IO ModSummary) -> ModuleGraph -> IO ModuleGraph
+mapMGM f = mgMapM $ \mni -> case mni of
+  ModuleNodeCompile ms -> ModuleNodeCompile <$> f ms
+  _ -> pure mni
+
+#else
+
+-- | Like 'mapMG', but monadic.
+mapMGM :: (ModSummary -> IO ModSummary) -> ModuleGraph -> IO ModuleGraph
+mapMGM f mg = do
+  mgns <- forM (mg_mss mg) $ \mgn -> case mgn of
+      ModuleNode deps ms  -> ModuleNode deps <$> f ms
+      _ -> pure mgn
+  pure mg { mg_mss = mgns }
+
+#endif
 
 #endif
